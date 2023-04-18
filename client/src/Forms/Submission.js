d2610a6e8f1d2f862fe86aafbebffbca046ff351
Karan Pargal <karanpargal007@gmail.com>
2023-04-18 15:01:00 +0530

    Add placeholders

--- client/src/Forms/Submission.js
+++ client/src/Forms/Submission.js
@@ -6,7 +6,7 @@ const Submission = () => {
     <div class="  m-auto max-w-2xl pt-20 h-72  text-black   ">
       <div class=" flex flex-col shadow-2xl shadow-violet-300 gap-4 border-fuchsia-600 border-2 justify-center item-center text-white  rounded px-10 pt-8 pb-8 ">
         <h1 className="block text-black text-2xl font-bold mb-2">
-          Submit Your project
+          Submit your bounty
         </h1>
         <div class="mb-4 mt-2 ">
           <label
@@ -31,7 +31,7 @@ const Submission = () => {
             className=" appearance-none mb-4 border rounded w-full py-2 px-3 text-gray-700 leading-tight "
             id="description"
             type="text"
-            placeholder=""
+            placeholder="xyz@gmail.com"
           ></input>
           
           <label
@@ -44,7 +44,7 @@ const Submission = () => {
             className=" appearance-none mb-4 border rounded w-full py-2 px-3 text-gray-700 leading-tight "
             id="tags"
             type="text"
-            placeholder=""
+            placeholder="Your submission link (GitHub/Twitter/Medium)"
           ></input>
           <label
             class="block text-black text-lg font-semibold mb-2"
@@ -56,7 +56,7 @@ const Submission = () => {
             className=" appearance-none mb-4  border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="walletAddress"
             type="text"
-            placeholder=""
+            placeholder="0x.."
           ></input>
           <button className="text-black shadow-md shadow-violet-300 hover:text-white transition ease-in-out delay-150 bg-white hover:-translate-y-1  hover:scale-100 hover:bg-violet-400 duration-300 text-lg font-semibold rounded-sm  outline  outline-1 outline-fuchsia-600 px-8" 
             >
