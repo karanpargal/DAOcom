8ac31dec7b6c1dd0087f774f280ec6e8f1f2a6ca
Karan Pargal <karanpargal007@gmail.com>
2023-04-18 16:37:47 +0530

    CORS

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://daocom-a703d9.spheron.app/"]
+    origin: "*"
   })
 );
 
